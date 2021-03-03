03520bcbe518524c36e7fab65839addc2889063e
fifty-six <ybham6@gmail.com>
2021-03-02 19:08:26 -0500

    Remove KeyValuePair<TKey, TValue> deconstruct
    No longer necessary with .NET 4.

--- Vasi/Deconstructor.cs
+++ Vasi/Deconstructor.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
 
@@ -10,10 +9,5 @@ namespace Vasi
         public static void Deconstruct(this Vector3 v, out float x, out float y, out float z) => (x, y, z) = (v.x, v.y, v.z);
 
         public static void Desconstruct(this Vector2 v, out float x, out float y) => (x, y) = (v.x, v.y);
-        
-        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> self, out TKey key, out TValue value)
-        {
-            (key, value) = (self.Key, self.Value);
-        }
     }
 }
